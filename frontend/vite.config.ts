eb70b8b3cbe7b4fbf5d3433dd50538681aa80444
xuequnhui <2370839713@qq.com>
2026-04-06 00:18:49 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 5173,
+    host: '0.0.0.0',
+    hmr: {
+      clientPort: 443,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
